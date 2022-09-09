6b561d4a1ae2964f7c67fbfe3582185f60570d16
snehalatha2111 <94513288+snehalatha2111@users.noreply.github.com>
2022-09-09 15:52:56 +0530

    Round Robin.c file is created

---
+++ oslab/exp 1/Round Robin.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: M snehalatha
+   Roll No: 204G1A0598
+   Title of the Experiment: CPU Scheduling Using Round Robin Algorithm
+   Date of Creation: 09-09-2022
+   Date of Execution: 09-09-2022
+*/
