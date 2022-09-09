559b5d20a6c6b4d3bc28d1b449f146a7a43c61e4
snehalatha2111 <94513288+snehalatha2111@users.noreply.github.com>
2022-09-09 15:07:57 +0530

    Create FCFS.c
    FCFS.c file is created

---
+++ oslab/exp 1/FCFS.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: M.snehalatha
+   Roll No: 204G1A0598
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 09-09-2022
+   Date of Execution: 09-09-2022
+*/
