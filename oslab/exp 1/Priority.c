808ba8e0a501e4518ccd47868ee0995fed277b8f
snehalatha2111 <94513288+snehalatha2111@users.noreply.github.com>
2022-09-09 16:20:38 +0530

    priority.c file is created

---
+++ oslab/exp 1/Priority.c
@@ -0,0 +1,7 @@
+ 
+/* Name Of the Candidate: m sneha latha
+   Roll No: 204G1A0598
+   Title of the Experiment: CPU Scheduling Using Priority Algorithm
+   Date of Creation: 09-09-2022
+   Date of Execution: 09-09-2022
+*/
